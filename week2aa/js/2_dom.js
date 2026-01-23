c79f7d62b846e42495d5916615dd3b7b2063e32d
3andman <230266410+3andman@users.noreply.github.com>
2026-01-23 13:21:29 -0500

    .

---
+++ week2aa/js/2_dom.js
@@ -0,0 +1,28 @@
+window.onload = setup;
+function setup() {
+  //     console.log("running setup");
+
+  //     // let second_element = document.getElementById("sabine");
+  //     // let second_again = document.querySelector("#two")
+  //     // console.log(second_element);
+  //     // console.log(second_again);
+
+  //     let elements = document.getElementsByTagName("div");
+  //     console.log(elements[elements.length - 1]);
+
+  //     for (let i =0, i< elements.length; i++) {
+  //         console.log(elements[i])
+  //     }
+
+  // let firstDiv = document.querySelector("div")
+  // let allDivs = document.querySelectorAll("div")
+
+  //     let elementsWithClass = document.getElementsByClassName(".square_shape");
+
+  //     let allElementsWithClass = document.querySelectorAll(".square_shape")
+
+  let firstEl = document.querySelector(".square_shape");
+    let htmlOfEl = firstEl.innerHTML;
+    console.log(firstEl)
+    console.log(htmlOfEl)
+}
---
+++ week2aa/js/helloWorld.js
@@ -0,0 +1,8 @@
+
+//window object has event called onload
+window.onload = setup;
+//function runs when page loads
+function setup() {
+  console.log("running setup");
+  document.write("HELLO WORLD AFTER LOAD IN FUNCTION");
+}
